1af5d161f9fcc9bac5b563343ea74ad15c1cb105
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2012-06-06 22:33:18 +0900

    embed output

--- clojure/stm.clj
+++ clojure/stm.clj
@@ -16,7 +16,7 @@
   (open "01")
   (gate "11")
   (ref-set rx 1)
-  (println "modified ref rx: 1")
+  (println "modified ref rx = 1")
   (open "03")
   (gate "12")
   (println "finish transaction 1")
@@ -33,7 +33,7 @@
   (gate "21")
   (println "transaction 2 trying to write")
   (ref-set rx 2)
-  (println "modified ref rx: 2")
+  (println "modified ref rx = 2")
   (gate "22")
   (println "finish transaction 2")
   ))
@@ -60,3 +60,54 @@
 
 (open "22")
 
+(comment (output checked by coderunner)
+start transaction 1
+thread t1 opened gate 01
+start transaction 2
+thread t2 closed gate 21
+thread t2 opened gate 02
+thread t0 passed the gate 01
+thread t0 passed the gate 02
+thread t0 opened gate 11 and waiting until gate 03 is open
+thread t1 passed the gate 11
+modified ref rx = 1
+thread t1 opened gate 03
+thread t0 passed the gate 03
+thread t0 closed gate 02
+thread t0 opened gate 21 and waiting until gate 02 is open
+thread t2 passed the gate 21
+transaction 2 trying to write
+start transaction 2
+thread t2 closed gate 21
+thread t2 opened gate 02
+thread t0 passed the gate 02
+thread t0 closed gate 02
+thread t0 opened gate 21 and waiting until gate 02 is open
+thread t2 passed the gate 21
+transaction 2 trying to write
+start transaction 2
+thread t2 closed gate 21
+thread t2 opened gate 02
+thread t0 passed the gate 02
+thread t0 closed gate 02
+thread t0 opened gate 12 and waiting until gate 04 is open
+thread t1 passed the gate 12
+finish transaction 1
+thread t1 opened gate 04
+thread t0 passed the gate 04
+thread t0 opened gate 21 and waiting until gate 02 is open
+thread t2 passed the gate 21
+transaction 2 trying to write
+start transaction 2
+thread t2 closed gate 21
+thread t2 opened gate 02
+thread t0 passed the gate 02
+thread t0 opened gate 21 and waiting until gate 02 is open
+thread t0 passed the gate 02
+thread t0 opened gate 22
+thread t2 passed the gate 21
+transaction 2 trying to write
+modified ref rx = 2
+thread t2 passed the gate 22
+finish transaction 2
+(end of comment))
--- clojure/test.py
+++ clojure/test.py
@@ -2,56 +2,6 @@ import sys
 sys.path.insert(0, "../coderunner")
 from coderunner import test, Clojure, main
 
-test(Clojure, "stm.clj", """
-start transaction 1
-thread t1 opened gate 01
-start transaction 2
-thread t2 closed gate 21
-thread t2 opened gate 02
-thread t0 passed the gate 01
-thread t0 passed the gate 02
-thread t0 opened gate 11 and waiting until gate 03 is open
-thread t1 passed the gate 11
-modified ref rx: 1
-thread t1 opened gate 03
-thread t0 passed the gate 03
-thread t0 closed gate 02
-thread t0 opened gate 21 and waiting until gate 02 is open
-thread t2 passed the gate 21
-transaction 2 trying to write
-start transaction 2
-thread t2 closed gate 21
-thread t2 opened gate 02
-thread t0 passed the gate 02
-thread t0 closed gate 02
-thread t0 opened gate 21 and waiting until gate 02 is open
-thread t2 passed the gate 21
-transaction 2 trying to write
-start transaction 2
-thread t2 closed gate 21
-thread t2 opened gate 02
-thread t0 passed the gate 02
-thread t0 closed gate 02
-thread t0 opened gate 12 and waiting until gate 04 is open
-thread t1 passed the gate 12
-finish transaction 1
-thread t1 opened gate 04
-thread t0 passed the gate 04
-thread t0 opened gate 21 and waiting until gate 02 is open
-thread t2 passed the gate 21
-transaction 2 trying to write
-start transaction 2
-thread t2 closed gate 21
-thread t2 opened gate 02
-thread t0 passed the gate 02
-thread t0 opened gate 21 and waiting until gate 02 is open
-thread t0 passed the gate 02
-thread t0 opened gate 22
-thread t2 passed the gate 21
-transaction 2 trying to write
-modified ref rx: 2
-thread t2 passed the gate 22
-finish transaction 2
-""", is_file=True)
+test(Clojure, "stm.clj", is_file=True, is_embedded_output=True)
 
 main()
