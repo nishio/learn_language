7c7199fa321e254702bdab5be7e94238efc56cf4
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2012-06-07 19:25:07 +0900

    add atom test

--- clojure/atom.clj
+++ clojure/atom.clj
@@ -1,64 +1,47 @@
 ;
 ; Atomの挙動を観察する
 ; 複数のAtomの変更をdosyncで囲っても、別のスレッドからは不整合な状態が見える
-
-; スレッドを作るマクロ
-; http://code.google.com/p/javaee-study/source/browse/trunk/clojure-concurrency/concurrency.clj?r=5
-(import '(java.lang Thread))
-(defmacro with-new-thread [& body]
-  `(.start (Thread. (fn [] ~@body))))
-
-; 指定された値がTrueになるまでブロックするチェックポイント
-(def dict (new java.util.Hashtable))
-
-(defn checkpoint [key]
-  (while (not (.get dict key))
-    (Thread/sleep 100)))
-
-(defn open [key]
-  (.put dict key true)
-  (println "open" key))
-
-(defn open-wait [key1 key2]
-  (println "open gate" key1 "and wait until gate" key2 "open")
-  (open key1)
-  (checkpoint key2))
+(require 'util)
 
 ; var
 (def x (atom 0))
 (def y (atom 0))
+(setThreadName "t0")
 
 (with-new-thread
- (dosync ; no meaning because x and y is not ref but atom
-  (checkpoint "1")
-  (reset! x 1)
-  (open "2")
+ (setThreadName "t1")
 
-  (checkpoint "3")
+ (dosync ; no meaning because x and y is not ref but atom
+  (gate "1")
+  (reset! x 1)
+  (open-wait "2" "3")
   (reset! y 1)
   (open "4")))
 
 ; before (reset! x 1)
 (println "x, y =" @x @y)
 
-(open "1")
-(checkpoint "2")
+(open-wait "1" "2")
 
 ; after (reset! x 1),  before (reset! y 1)
-(println "x, y =" @x @y)
+(println "x, y =" @x @y "(inconsistent)")
 
-(open "3")
-(checkpoint "4")
+(open-wait "3" "4")
 
 ; after (reset! x 1), (reset! y 1)
 (println "x, y =" @x @y)
 
-;output -----
+(comment (output checked by coderunner)
 x, y = 0 0
-open 1
-open 2
-x, y = 1 0
-open 3
-open 4
+thread t0 opened gate 1 and waiting until gate 2 is open
+thread t1 passed the gate 1
+thread t1 opened gate 2 and waiting until gate 3 is open
+thread t0 passed the gate 2
+x, y = 1 0 (inconsistent)
+thread t0 opened gate 3 and waiting until gate 4 is open
+thread t1 passed the gate 3
+thread t1 opened gate 4
+thread t0 passed the gate 4
 x, y = 1 1
+(end of comment))
 
