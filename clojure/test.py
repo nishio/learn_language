01a7333f3087a60fbe66f212cc3dacd548f13e10
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2012-06-07 18:41:08 +0900

    add var.clj

--- clojure/test.py
+++ clojure/test.py
@@ -4,5 +4,6 @@ from coderunner import test, Clojure, main
 
 test(Clojure, "stm.clj", is_file=True, is_embedded_output=True)
 test(Clojure, "stm2.clj", is_file=True, is_embedded_output=True)
+test(Clojure, "var.clj", is_file=True, is_embedded_output=True)
 
 main()
--- clojure/var.clj
+++ clojure/var.clj
@@ -1,49 +1,44 @@
 ;
 ; STMの挙動を観察する2
 ; varの動的スコープと静的スコープ、それがスレッドローカルであること
-
-; スレッドを作るマクロ
-; http://code.google.com/p/javaee-study/source/browse/trunk/clojure-concurrency/concurrency.clj?r=5
-(import '(java.lang Thread))
-(defmacro with-new-thread [& body]
-  `(.start (Thread. (fn [] ~@body))))
-
-; 指定された値がTrueになるまでブロックするチェックポイント
-(def dict (new java.util.Hashtable))
-
-(defn checkpoint [key]
-  (while (not (.get dict key))
-    (Thread/sleep 100)))
-
-(defn open [key]
-  (.put dict key true)
-  (println "open" key))
-
-(defn open-wait [key1 key2]
-  (println "open gate" key1 "and wait until gate" key2 "open")
-  (open key1)
-  (checkpoint key2))
+(require 'util)
 
 ; var
 (def v 42) ; static scope
-(def ^:dynamic w 42)
+(def ^:dynamic w 42) ; dynamic scope
 
 (defn show_v [] (println "in func, v is" v))
 (defn show_w [] (println "in func, w is" w))
+(setThreadName "t0")
 
 (with-new-thread
+ (setThreadName "t1")
  (let [v 142]
       (binding [w 242]
                (println "in thread1")
-               (println "v is" v)
+               (println "  v is" v)
                (show_v)
-               (println "w is" w)
+               (println "  w is" w)
                (show_w)
                (open-wait "1" "2"))))
 
-(checkpoint "1")
+(gate "1")
 (println "in thread 0")
-(println "v is" v)
-(println "w is" w)
+(println "  v is" v)
+(println "  w is" w)
 (open "2")
 
+(comment (output checked by coderunner)
+in thread1
+  v is 142
+in func, v is 42
+  w is 242
+in func, w is 242
+thread t1 opened gate 1 and waiting until gate 2 is open
+thread t0 passed the gate 1
+in thread 0
+  v is 42
+  w is 42
+thread t0 opened gate 2
+thread t1 passed the gate 2
+(end of comment))
