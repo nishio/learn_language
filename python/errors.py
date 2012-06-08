bd6ab3ed60cd02ad9fba95baf392be9246ad9e18
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2012-06-08 17:36:15 +0900

    add tests

--- python/errors.py
+++ python/errors.py
@@ -53,6 +53,39 @@ print "こんにちは"
 """)
 
 test(Python, """
+def foo(): pass
+
+foo(1)
+""", """
+Traceback (most recent call last):
+  File "tmp.py", line 3, in <module>
+    foo(1)
+TypeError: foo() takes no arguments (1 given)
+""")
+
+test(Python, """
+def foo(x=1): pass
+
+foo(y=1)
+""", """
+Traceback (most recent call last):
+  File "tmp.py", line 3, in <module>
+    foo(y=1)
+TypeError: foo() got an unexpected keyword argument 'y'
+""")
+
+test(Python, """
+def foo(x, y, z=0): pass
+
+foo(1, z=1)
+""", """
+Traceback (most recent call last):
+  File "tmp.py", line 3, in <module>
+    foo(1, z=1)
+TypeError: foo() takes at least 2 arguments (2 given)
+""")
+
+test(Python, """
 def foo(x=0, **kw):
     print x, kw
 
@@ -62,6 +95,87 @@ Traceback (most recent call last):
   File "tmp.py", line 4, in <module>
     foo(1, x=2)
 TypeError: foo() got multiple values for keyword argument 'x'
+""")
+
+test(Python, """
+''[0]
+""", """
+Traceback (most recent call last):
+  File "tmp.py", line 1, in <module>
+    ''[0]
+IndexError: string index out of range
+""")
+
+test(Python, """
+'' + 0
+""", """
+Traceback (most recent call last):
+  File "tmp.py", line 1, in <module>
+    '' + 0
+TypeError: cannot concatenate 'str' and 'int' objects
+""")
+
+test(Python, """
+0 + ''
+""", """
+Traceback (most recent call last):
+  File "tmp.py", line 1, in <module>
+    0 + ''
+TypeError: unsupported operand type(s) for +: 'int' and 'str'
+""")
+
+test(Python, """
+-''
+""", """
+Traceback (most recent call last):
+  File "tmp.py", line 1, in <module>
+    -''
+TypeError: bad operand type for unary -: 'str'
+""")
+
+test(Python, """
+x
+""", """
+Traceback (most recent call last):
+  File "tmp.py", line 1, in <module>
+    x
+NameError: name 'x' is not defined
+""")
+
+test(Python, r"""
+u'' + '\xFF'
+""", r"""
+Traceback (most recent call last):
+  File "tmp.py", line 1, in <module>
+    u'' + '\xFF'
+UnicodeDecodeError: 'ascii' codec can't decode byte 0xff in position 0: ordinal not in range(128)
+""")
+
+test(Python, r"""
+{}['x']
+""", """
+Traceback (most recent call last):
+  File "tmp.py", line 1, in <module>
+    {}['x']
+KeyError: 'x'
+""")
+
+test(Python, r"""
+[].x
+""", """
+Traceback (most recent call last):
+  File "tmp.py", line 1, in <module>
+    [].x
+AttributeError: 'list' object has no attribute 'x'
+""")
+
+test(Python, r"""
+import x
+""", """
+Traceback (most recent call last):
+  File "tmp.py", line 1, in <module>
+    import x
+ImportError: No module named x
 """)
 
 main()
