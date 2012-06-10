854192a715c05cfc389781addf04419841167aa5
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2012-06-10 16:17:47 +0900

    add python3.0 support

--- coderunner/coderunner.py
+++ coderunner/coderunner.py
@@ -229,6 +229,11 @@ class Python27(_Python):
     human_name = "Python2.7"
 
 
+class Python30(_Python):
+    bin = "python3.0"
+    human_name = "Python3.0"
+
+
 class Python(Python27):
     human_name = "Python"
 
