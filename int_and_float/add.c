14ecc7037847d5ce013c3db64013b2d6d0864506
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2012-06-18 03:14:08 +0900

    update test

--- int_and_float/add.c
+++ int_and_float/add.c
@@ -23,7 +23,7 @@ void show(float x)
   printf("Hexadecimal value 0x%X in memory,\n", i);
   printf("  %s as binary,\n", s);
   printf("  %g as float\n", x);
-  printf("  %u as uint\n", i);
+  printf("  %u as uint\n\n", i);
 }
 
 int main(int argc, char *argv[])
@@ -37,12 +37,16 @@ int main(int argc, char *argv[])
   show(x);
   show(y);
 
+  printf("Add as float:\n");
+  show(x + y);
+
+  printf("Add as integer:\n");
   memcpy(&ix, &x, sizeof(ix));
   memcpy(&iy, &y, sizeof(ix));
   ix += iy; // add as integer
   memcpy(&x, &ix, sizeof(ix));
-
   show(x);
+
   return 0;
 }
 
@@ -51,10 +55,19 @@ Hexadecimal value 0x40400000 in memory,
   01000000010000000000000000000000 as binary,
   3 as float
   1077936128 as uint
+
 Hexadecimal value 0x40E00000 in memory,
   01000000111000000000000000000000 as binary,
   7 as float
   1088421888 as uint
+
+Add as float:
+Hexadecimal value 0x41200000 in memory,
+  01000001001000000000000000000000 as binary,
+  10 as float
+  1092616192 as uint
+
+Add as integer:
 Hexadecimal value 0x81200000 in memory,
   10000001001000000000000000000000 as binary,
   -2.93874e-38 as float
