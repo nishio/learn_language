96c2ebb604deb5057c39b18159de9487ca39b24b
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2012-08-21 15:30:01 +0900

    add another with-statement sample

--- with-block/test.py
+++ with-block/test.py
@@ -12,5 +12,7 @@ from coderunner.coderunner import test, Python, Java7, CSharp, main
 test(Java7, "TryWithResource.java", is_file=True, is_embedded_output=True)
 test(CSharp, "Using.cs", is_file=True, is_embedded_output=True)
 test(Python, "with_statement.py", is_file=True, is_embedded_output=True)
+test(Python, "with_statement2.py", is_file=True, is_embedded_output=True)
+
 
 main()
